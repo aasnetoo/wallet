88c29095feaa1c150337c5e06fbce87efaa89aab
Antonio Neto <antonioneto.mec@gmail.com>
2023-02-20 16:01:21 -0300

    Fixed Error in UserControllerTest

--- src/main/java/com/wallet/dto/UserDTO.java
+++ src/main/java/com/wallet/dto/UserDTO.java
@@ -11,7 +11,7 @@ public class UserDTO {
 
     private Long id;
     private String name;
-    
+
     private String password;
     private String email;
 
--- src/test/java/com/wallet/controller/UserControllerTest.java
+++ src/test/java/com/wallet/controller/UserControllerTest.java
@@ -5,10 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.wallet.dto.UserDTO;
 import com.wallet.entity.UserEntity;
 import com.wallet.service.impl.UserServiceImpl;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -18,8 +23,8 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@DataJpaTest
 public class UserControllerTest {
 
     private static final String EMAIL = "email@teste.com";
@@ -33,11 +38,13 @@ public class UserControllerTest {
     @Autowired
     MockMvc mvc;
 
+    @Test
     public void testSave() throws Exception {
+        BDDMockito.given(userService.saveUser(Mockito.any(UserEntity.class))).willReturn(getMockUser());
+
         mvc.perform(MockMvcRequestBuilders.post(URL).content(getJsonPayload())
                 .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
+                .accept(MediaType.APPLICATION_JSON)).andExpect(status().isCreated());
 
 
 
